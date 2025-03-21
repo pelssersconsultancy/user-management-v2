d717d31b1363b842aba6374ca82a1996cc1e9884
Robby Pelssers <pelssersconsultancy@gmail.com>
2025-03-21 08:56:29 +0100

    feat: add dependencies on use.core from query and command projects

--- user.core/src/main/java/com/springbank/user/core/configuration/AxonConfig.java
+++ user.core/src/main/java/com/springbank/user/core/configuration/AxonConfig.java
@@ -5,13 +5,19 @@ import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
 
 import org.axonframework.eventhandling.tokenstore.TokenStore;
+import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
+import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
+import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.extensions.mongo.DefaultMongoTemplate;
 import org.axonframework.extensions.mongo.MongoTemplate;
+import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoEventStorageEngine;
 import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoFactory;
 import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoSettingsFactory;
 import org.axonframework.extensions.mongo.eventsourcing.tokenstore.MongoTokenStore;
 import org.axonframework.serialization.Serializer;
 import org.axonframework.serialization.json.JacksonSerializer;
+import org.axonframework.spring.config.AxonConfiguration;
+import org.axonframework.springboot.autoconfig.AxonAutoConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -59,5 +65,23 @@ public class AxonConfig {
     public TokenStore tokenStore(Serializer serializer) {
         return MongoTokenStore.builder().mongoTemplate(axonMongoTemplate()).serializer(serializer).build();
     }
+
+    @Bean
+    public EventStorageEngine storageEngine(MongoClient client) {
+        return MongoEventStorageEngine.builder()
+                .mongoTemplate(DefaultMongoTemplate.builder()
+                        .mongoDatabase(client)
+                        .build())
+                .build();
+    }
+
+    // TODO: this code is not exactly the same (see video 13)
+    @Bean
+    public EmbeddedEventStore eventStore(EventStorageEngine storageEngine) {
+        return EmbeddedEventStore.builder()
+                .storageEngine(storageEngine)
+                .build();
+    }
+
 
 }
